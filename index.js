bcff10dd8e256bf6e230148b7035b52e95009896
Babar Khan <babarkhan138@gmail.com>
2020-03-11 20:10:41 +0500

    Initial Author Routes

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
+// Routes
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 // Set Template Engine, Views, layout.
 app.set("view engine", "ejs");
@@ -15,15 +18,19 @@ app.set('layout', 'layouts/layout');
 
 app.use(expressLayouts);
 app.use(express.static('pubic'));
-app.use('/', indexRouter);
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 // Database
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
+
+// Routes
+app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(
     process.env.PORT || 3000,
